4b3800dc5462e2eef812559df6371e44ee417dac
Abhishek <abhishek130199@gmail.com>
2024-02-25 11:13:50 +0530

    01_variables added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const password=123456;
+let username="im_abhishek";
+let email="abhishek@gmail.com";
+let phoneNumber=876545678;
+let alpha;
+
+//prefer kiya jata hai ki hum let ka use karey var ka use na karey
+//agar hum const ko value assign nahi karenge then its throw error
+//agar hum let ko value assign nahi karenge then its give undefined
+
+console.table([password, username, email, phoneNumber,alpha]);
+
