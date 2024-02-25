a36bc735eae482e31a15e5fedda043c43d898a99
Abhishek <abhishek130199@gmail.com>
2024-02-25 11:48:27 +0530

    add datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,7 +4,7 @@ let email="abhishek@gmail.com";
 let phoneNumber=876545678;
 let alpha;
 
-//prefer kiya jata hai ki hum let ka use karey var ka use na karey
+//prefer kiya jata hai ki hum let ka use karey var ka use na karey[because of issue block and function scope]
 //agar hum const ko value assign nahi karenge then its throw error
 //agar hum let ko value assign nahi karenge then its give undefined
 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+let names="rohit";
+let age=24;
+let isEducated=true;
+let isMarried=null;
+let isFather
+let isHouse="";
+
+console.table([names, age, isEducated, isFather, isMarried, isHouse]);
+
+//String
+//boolean
+//number
+//null=> standalone value
+//undefined=> means abhi value defined nahi hui hai
+//symbol=> unique
+//object
+
+//jab hum kisi variable m koi value nahi assign kartey hai to wo variable undefined hota hai iska means humne abhi uss variable me koi value assign nahi ki hai.
+//Jab hum kisi variable ko null assign kartey hai t uska means hum usko khali rakh rahey hai.
+
+//null vs undefined vs "" yeh teeno differ hai.
+
+
+
+console.table([typeof(names), typeof(age), typeof(isEducated), typeof(isFather), typeof(isMarried), typeof(isHouse)]);
+
+//agar hum datatype check karna chatey hai then humko typeof() function use karna padega.
+//Null ka datatype object aata hai magar undefined ka datatype undefined hi hota hai.
+
+
