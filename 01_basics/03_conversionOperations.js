3e60c242db70da4ab26b6fc39f2248eb991d5488
Abhishek <abhishek130199@gmail.com>
2024-02-25 12:13:15 +0530

    conversion of dt

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+let life = "20";
+
+life= Number(life);
+
+console.log(typeof(life), life);
+
+//Jab bhi hum kisis ek datatype ko kisi doosrey datatype m change karna chtey ho then hum uskey agey us datatype ka name likh detey hai.
+
+//Datatype===>number
+// ie) life= Number(life);=> life varibale changed in number datatype
+// agar hum null ko number m change karengey then wo 0 m change hoga
+// agar hum undefined ko number m change karengey then wo NaN m change hoga
+// agar hum true ko number m change karengey then wo 1 m change hoga
+// agar hum false ko number m change karengey then wo 0 m change hoga
+// agar hum "20life" is tarah ki string ko number m change karengey then wo NaN m change hoga
+
+
+let names="gjhh";
+names=Boolean(names);
+console.log(typeof(names), names);
+//Datatype===>boolean
+//1=>true
+//0=>false
+//"dfgghjkk"=>true
+//""=>false
+
